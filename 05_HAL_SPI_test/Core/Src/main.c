64154251514da6d490a38bc86939dd2f6e812c6a
Charan <mhchandu26@gmail.com>
2025-10-29 14:50:36 +0530

    SPI Polling

---
+++ 05_HAL_SPI_test/Core/Src/main.c
@@ -0,0 +1,84 @@
+/*
+ * This program uses SPI1 on STM32 to transmit and receive 10 bytes of data.
+ */
+
+#include "main.h"
+
+SPI_HandleTypeDef hspi1;   // SPI1 handle
+
+
+uint8_t tx_buffer[10] = {10, 20, 40, 50, 60, 70, 90, 90, 100, 110}; // Data to send
+uint8_t rx_buffer[10]; // Buffer to store received data
+
+void SystemClock_Config(void);     // Configures system clock source and speed
+static void MX_SPI1_Init(void);    // Initializes SPI1 for master mode operation
+
+int main(void)
+{
+  HAL_Init();                 // Initialize HAL library
+  SystemClock_Config();       // Configure system clock
+  MX_SPI1_Init();             // Initialize SPI1
+
+  // Send tx_buffer and receive into rx_buffer using SPI1
+  HAL_SPI_TransmitReceive(&hspi1, tx_buffer, rx_buffer, 10, 100);
+
+  while (1)
+  {
+
+  }
+}
+
+void SystemClock_Config(void) // Sets up HSI oscillator, PLL, and bus clocks
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 1;
+  RCC_OscInitStruct.PLL.PLLN = 10;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV7;
+  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+  HAL_RCC_OscConfig(&RCC_OscInitStruct);
+
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+                              | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4);
+}
+
+static void MX_SPI1_Init(void) // Configures SPI1 in master mode, 8-bit, full-duplex
+{
+  hspi1.Instance = SPI1;
+  hspi1.Init.Mode = SPI_MODE_MASTER;
+  hspi1.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi1.Init.NSS = SPI_NSS_SOFT;
+  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_64;
+  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi1.Init.CRCPolynomial = 7;
+  hspi1.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
+  hspi1.Init.NSSPMode = SPI_NSS_PULSE_ENABLE;
+  HAL_SPI_Init(&hspi1);
+}
+
+
+
+void Error_Handler(void) // Stops execution if an error occurs
+{
+  __disable_irq();
+  while (1) { }
+}
